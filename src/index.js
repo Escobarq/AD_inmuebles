a48f47728e31b42f4db89c6aef821bc22fe86a73
Arenasel <juandavidarenasm2004@gmail.com>
2023-11-16 21:14:18 -0500

    diseño de interfaz prototipo

--- src/index.js
+++ src/index.js
@@ -1,62 +1,14 @@
-const { app , BrowserWindow, Menu} = require ('electron');
-const path = require('path');
-const url = require('url');
+const { app } = require('electron');
+const { createWindow } = require('./main');
 
-/*reiniciara cuando no este en produccion */
-if (process.env.NODE_ENV !== 'production') {
-    require('electron-reload')(__dirname,{
-     electron: path.join(__dirname, '../node_modules','.bin','electron')
-    })
-};
-/* */
+app.whenReady().then(createWindow);
 
-let mainWindow 
-let newProductWindow
-
-/*inicio de aplicacion index.html*/
-app.on('ready', () => {
-    mainWindow = new BrowserWindow({});
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname,'views/index.html'),
-        protocol: 'file',
-        slashes: true
-    }))
-/**/
-    
-/* Uso del  menu */
-   const mainMenu =  Menu.buildFromTemplate(templateMenu)
-   Menu.setApplicationMenu(mainMenu);
-/**/
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
 });
 
-/*Crear nueva ventana*/
-function createNewProductWindow(){
-    newProductWindow = new BrowserWindow({  
-        width:400,
-        height:400,
-        title: 'New Product'
-    });
-    newProductWindow.setMenu(null);
-    newProductWindow.loadURL(url.format({
-        pathname: path.join(__dirname,'views/new-product.html'),
-        protocol: 'file',
-        slashes: true
-    }))
-}
-/**/
-
-/* Creacion del Menu */
-const templateMenu = [
-    {
-        label: 'File',
-        submenu: [{
-
-            label:'New Product',
-            accelerator: 'Ctrl+N',
-            click(){
-                createNewProductWindow()
-            }
-        }]
-    }
-];
-/**/
+app.on('activate', function () {
+  if (window === null) {
+    createWindow();
+  }
+});
---
+++ src/main.js
@@ -0,0 +1,26 @@
+const { BrowserWindow } = require('electron');
+
+let window;
+
+function createWindow() {
+  window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  window.loadFile('views/index.html');
+
+
+  window.on('closed', function () {
+    window = null;
+  });
+
+  window.setMenu(null);
+
+  return window;
+}
+
+module.exports = { createWindow };
