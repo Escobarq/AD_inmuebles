c02d7da7f472d0df2c607eaf26edc1571b0938c8
JuanDavid <140362363+Escobarq@users.noreply.github.com>
2024-03-20 17:27:05 -0500

    yo JD escobar 17/03/2024 arreglado backend terminado

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const fs = require('fs');
 
 let connection;
 let databaseConfig;
@@ -6,6 +7,8 @@ let databaseConfig;
 function configureDatabase({ host, user, password, database }) {
   databaseConfig = { host, user, password, database };
 
+  const configData = JSON.stringify(databaseConfig, null, 2);
+  fs.writeFileSync('./config.json', configData);
   connection = mysql.createConnection({
     host,
     user,
@@ -23,12 +26,33 @@ function configureDatabase({ host, user, password, database }) {
 }
 
 function getConnection() {
-  if (!connection) {
-    console.error('Error connecting to database:', );
+  try {
+    // Leer los datos de conexión desde el archivo config.json
+    const configData = fs.readFileSync('./config.json');
+    databaseConfig = JSON.parse(configData);
+  } catch (err) {
+    console.error('Error al leer el archivo de configuración:', err);
   }
+
+  if (!databaseConfig) {
+    console.error('Error: No se encontraron datos de conexión');
+    return null;
+  }
+
+  if (!connection) {
+    connection = mysql.createConnection(databaseConfig);
+
+    connection.connect((error) => {
+      if (error) {
+        console.error('Error connecting to database:', error);
+      } else {
+        console.log('Connected to MySQL database');
+      }
+    });
+  }
+
   return connection;
 }
-
 function getDatabaseConfig() {
   return databaseConfig;
 }
